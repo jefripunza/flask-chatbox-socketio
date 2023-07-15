0599b57e0b34aa4b5e95cdbd531a1abd686c775d
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2023-07-15 10:36:04 +0700

    -2

--- main.py
+++ main.py
@@ -18,11 +18,9 @@ class LogFilter(logging.Filter):
 logging.getLogger("werkzeug").addFilter(LogFilter())
 
 app = Flask(__name__)
-CORS(app, origins='*')
 app.config['SECRET_KEY'] = 'secret_key'
-app.config['SESSION_COOKIE_SECURE'] = False
-app.config['SESSION_COOKIE_HTTPONLY'] = False
 socketio = SocketIO(app)
+CORS(app, origins='*')
 
 
 @app.route('/')
