8e03551c97ee95de8e55d9a7755885ba10cd048e
Jefri Herdi Triyanto <jefri.triyanto@ist.id>
2023-07-15 10:15:23 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# :rocket: Flask ChatBox SocketIO
+
+I created this service for the purpose of research and development regarding flask and websocket
+
+## :paperclip: Menu
+
+- :bulb: [Features](#bulb-features)
+- :hammer: [Project Setup](#hammer-project-setup)
+- :bug: [Known Bugs](https://github.com/jefripunza/flask-chatbox-socketio/issues)
+
+---
+
+## :bulb: Features
+
+| Tech     | link                                                                         | Description                                                    |
+|----------|------------------------------------------------------------------------------|----------------------------------------------------------------|
+| Python   | [doc](https://www.python.org/downloads/release/python-3113/ "Python 3.11.3") | language used                                                  |
+| PyCharm  | [doc](https://www.jetbrains.com/pycharm/download/ "PyCharm Download")        | IDE for Python                                                 |
+| Flask    | [doc](https://flask.palletsprojects.com/en/2.3.x/ "Flask Homepage")          | micro web framework written in Python                          |
+| SocketIO | [doc](https://socket.io/ "SocketIO Homepage")                                | Bidirectional and low-latency communication for every platform |
+
+<br/>
+
+## :hammer: Project Setup
+
+1. Clone this repo `git clone https://github.com/jefripunza/flask-chatbox-socketio`
+2. Open **PyCharm** and open folder project
+3. Install Virtual Environment & libraries
+
+- NOTE: if you not use PyCharm, run
+  `python -m pip install -r requirements.txt`
+
+4. Run App!!! use `python -m flask run`
+
+<br/>
+
+## 💫 License
+
+- Code and Contributions have **MIT License**
+
+_Copyright (c)
+2023 [Jefri Herdi Triyanto](http://github.com/jefripunza "My Github") ([@jefripunza](https://instagram.com/jefripunza "My Instagram"))_
---
+++ main.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, make_response, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_cors import CORS
+
+import time
+import uuid
+from flask.logging import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class LogFilter(logging.Filter):
+    def filter(self, record):
+        return not any(keyword in record.getMessage() for keyword in ["/socket.io", "transport"])
+
+
+logging.getLogger("werkzeug").addFilter(LogFilter())
+
+app = Flask(__name__)
+CORS(app)
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SESSION_COOKIE_SECURE'] = False
+app.config['SESSION_COOKIE_HTTPONLY'] = False
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    response = make_response(render_template('index.html'))
+    user_id = request.cookies.get('user_id')
+    if not user_id:
+        user_id = str(uuid.uuid4())
+        response.set_cookie('user_id', user_id)
+    return response
+
+
+@socketio.on('connect', namespace='/ws/client')
+def handle_connect():
+    user_id = request.cookies.get('user_id')
+    print("new user connect:", user_id)
+    join_room(user_id)
+
+
+@socketio.on('disconnect', namespace='/ws/client')
+def handle_disconnect():
+    user_id = request.cookies.get('user_id')
+    leave_room(user_id)
+
+
+typing_users = set()
+
+
+@socketio.on('message', namespace='/ws/client')
+def handle_message(receive):
+    user_id = request.cookies.get('user_id')
+    print("cookie:", user_id, "| message:", receive)
+    emit('message', receive, room=user_id)
+    message = receive["message"]
+    if message in ["ping", "hi", "hai", "halo", "mantap"] and user_id not in typing_users:
+        typing_users.add(user_id)
+        emit('typing_status', {"id": user_id, "typing": True}, room=user_id)
+        time.sleep(2)
+        typing_users.remove(user_id)
+        emit('typing_status', {"id": user_id, "typing": False}, room=user_id)
+        if message == "ping":
+            emit('message', {"message": "Pong!", "sender": "server"}, room=user_id)
+        elif message in ["hi", "hai", "hei", "halo", "hallo" "helo", "hello"]:
+            emit('message', {"message": "Hai!", "sender": "server"}, room=user_id)
+        elif message == "mantap":
+            emit('message', {"message": "Jiwa!", "sender": "server"}, room=user_id)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
